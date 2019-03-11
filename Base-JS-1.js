7f7c001bc814ac66aba62bbbfe00afdcfc84782f
Sébastien Jolly <seb_879@hotmail.fr>
2019-03-11 09:52:08 +0100

    index

---
+++ Base-JS-1.js
@@ -0,0 +1,10 @@
+/* hello-wilder.js */
+
+// Define some variables
+const nameMovie = 'Interstellar';
+const directorName = 'Christopher Nolan';
+const year = '2014'
+const message = nameMovie + ', réalisé par ' + directorName + ', est sorti en ' + year ;
+
+// Display a popup
+alert(message);
